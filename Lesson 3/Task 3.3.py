b3e5b3a057a1a0063c584225fbbb4fd965c9f736
yura775 <manilov.yura@gmail.com>
2021-04-19 21:45:51 +0300

    Write a program that has a variable with your name stored (in lowercase) and then asks for your name as input. The program should check if your input is equal to the stored name even if the given name has another case, e.g., if your input is “Anton” and the stored name is “anton”, it should return True.

---
+++ Lesson 3/Task 3.3.py
@@ -0,0 +1,7 @@
+real_name='chubaka'
+name=input('What is your name?')
+name=name.lower()
+if name==real_name:
+    print('Its time for you to go to barber!')
+else:
+    print('Who are you? We are not glad to see you here!')
