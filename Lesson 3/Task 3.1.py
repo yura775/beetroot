13404bb3e00964a13e4a64b8f5ba99d80b5b1882
yura775 <82705864+yura775@users.noreply.github.com>
2021-04-20 10:24:49 +0300

    Delete Lesson 3 directory

--- Lesson 3/Task 3.1.py
+++
@@ -1,7 +0,0 @@
-phrase = str(input('Make a sentence'))
-length = len(phrase)
-xlength = length - 2
-if length < 2:
-    print('Empty string')
-else:
-    print(phrase[0:2] + phrase[xlength:length])
--- Lesson 3/Task 3.2.py
+++
@@ -1,8 +0,0 @@
-phone=input('Please write down your number')
-if len(phone)!=10:
-    print('Your number must consist of 10 digits. Please try once again')
-elif phone.isnumeric():
-    print('Thank you! We will call you very soon')
-else:
-    print('Hmm, looks like your phone number contains letters... please try once more')
-
