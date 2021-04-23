d947d35c255fb9dbeba6d80f446faa3eeca77770
yura775 <manilov.yura@gmail.com>
2021-04-23 19:23:45 +0300

    Lesson 5, task 3

--- Lesson 5/Task 5.3.py
+++ Lesson 5/Task 5.3.py
@@ -2,10 +2,10 @@ import random
 
 f=random.sample(range(1,101),100)
 e = 0
-i = []
 n = f[e]
-while e>100:
+i = []
+while e<=100:
     if n %7==0 and not n %5==0:
         i.append(n)
-        e=e+1
+    e=(e+1)
 print(i)
